0ccce58a6c993b69f7f0f2a92619ed5b2a774f50
Awab Omer <awab228@yahoo.com>
2022-07-30 07:57:48 +0200

    Test (#6)
    * Adds H2 in memory database for testing, and Adds the test for student Repository
    
    * Fixes H2 dialect to pass the test successfully

--- src/main/java/com/sgx/fullstack/student/Student.java
+++ src/main/java/com/sgx/fullstack/student/Student.java
@@ -36,4 +36,10 @@ public class Student {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Gender gender;
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
